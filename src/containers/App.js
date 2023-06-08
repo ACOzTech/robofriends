a45ffb0610883f324c918b6c8403387da038f94b
acoztech <40618349+ACOzTech@users.noreply.github.com>
2023-06-08 22:30:45 +1000

    Get users data using useEffect instead of own function to get async data and update state.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,36 +1,19 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 
-async function GetRobots() {
-  let resp = await fetch('https://jsonplaceholder.typicode.com/users');
-  let robots = await resp.json();
-  if (robots === undefined) {
-    return [];
-  } else {
-    return robots;
-  }
-}
-
-const robo = GetRobots(); 
-
 function App() {
   const [robots, setRobots] = useState([]);
   const [searchfield, setSearchField] = useState('');
 
-  if (robots === undefined || robots.length === 0) {
-    robo.then(users => {
-        setRobots(
-          ...robots,
-          users.map(user => {
-            return user
-          })
-        );
-      });
-  }
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setRobots(data))
+  }, []);
 
   const filteredRobots = robots.filter(robot => {
     return robot.name.toLowerCase().includes(searchfield.toLowerCase());
